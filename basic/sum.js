006024f044e8b230e3a993eea03c59c0827d26ad
ashleylai <ashleylai58@gmail.com>
2021-07-31 12:10:10 +0800

    add sum.js

---
+++ basic/sum.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+// 回傳 1 + 2 + ... + n 的結果
+function sum(n) {
+  let result = 0;
+  return result;
+}
+
+console.log(sum(1)); // 1
+console.log(sum(2)); // 3
+console.log(sum(10)); // 55
