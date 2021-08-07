08e4347d33f2466e1e94b5c6ff22868025b87939
ashleylai <ashleylai58@gmail.com>
2021-08-07 17:15:28 +0800

    crawler1 callback 增加錯誤處理

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -12,19 +12,21 @@ const moment = require("moment");
 // 讀檔案，把股票代碼讀進來
 // nodejs 內建的功能 -> 不需要 npm i
 const fs = require("fs");
-
 // 刷牙 -> 吃早餐
-
 fs.readFile("stock.txt", "utf8", (err, stockCode) => {
-  axios
-    .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-      params: {
-        response: "json",
-        date: moment().format("YYYYMMDD"),
-        stockNo: stockCode, // 長榮航空 2618  長榮航海王 2603
-      },
-    })
-    .then((response) => {
-      console.log(response.data.title);
-    });
+  if (err) {
+    console.error(err);
+  } else {
+    axios
+      .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+        params: {
+          response: "json",
+          date: moment().format("YYYYMMDD"),
+          stockNo: stockCode, // 長榮航空 2618  長榮航海王 2603
+        },
+      })
+      .then((response) => {
+        console.log(response.data.title);
+      });
+  }
 });
